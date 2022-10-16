2ffe71674abec081aaaee2c3125fc0bbbaa3ef9b
chicodevbr <chicodevbr@gmail.com>
2022-10-16 18:32:42 -0300

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by fjunior on 16/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
