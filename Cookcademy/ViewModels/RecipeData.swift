543443ddf9110e20eab85b0a9ff671b1a474e617
chicodevbr <chicodevbr@gmail.com>
2022-10-22 16:21:53 -0300

    refactoring recipe data

--- Cookcademy/ViewModels/RecipeData.swift
+++ Cookcademy/ViewModels/RecipeData.swift
@@ -8,14 +8,16 @@
 import Foundation
 
 class RecipeData: ObservableObject {
-  @Published var recipe = Recipe.testRecipes
+  @Published var recipes = Recipe.testRecipes
  
-  func recipes(for category: MainInformation.Category) -> [Recipe] {
-    var filteredRecipes = [Recipe]()
-    for recipe in recipeData.recipes {
+  func recipes(for category:
+               MainInformation.Category) -> [Recipe] {
+      var filteredRecipes = [Recipe]()
+      for recipe in recipes {
       if recipe.mainInformation.category == category {
         filteredRecipes.append(recipe)
       }
     }
+      return filteredRecipes
   }
 }
