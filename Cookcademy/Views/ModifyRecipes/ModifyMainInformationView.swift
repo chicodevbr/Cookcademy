cdc0c3238669004350ab7cd9b7cfaaf50901f326
chicodevbr <chicodevbr@gmail.com>
2022-10-26 23:11:51 -0300

    add form add recipe

---
+++ Cookcademy/Views/ModifyRecipes/ModifyMainInformationView.swift
@@ -0,0 +1,47 @@
+//
+//  ModifyMainInformationView.swift
+//  Cookcademy
+//
+//  Created by fjunior on 26/10/22.
+//
+
+import SwiftUI
+
+struct ModifyMainInformationView: View {
+  @Binding var mainInformation: MainInformation
+ 
+  var body: some View {
+    Form {
+      TextField("Recipe Name", text: $mainInformation.name)
+      TextField("Author", text: $mainInformation.author)
+      Section(header: Text("Description")) {
+        TextEditor(text: $mainInformation.description)
+      }
+      Picker(selection: $mainInformation.category, label:
+              HStack {
+                Text("Category")
+                Spacer()
+                Text(mainInformation.category.rawValue)
+              }) {
+        ForEach(MainInformation.Category.allCases,
+                id: \.self) { category in
+          Text(category.rawValue)
+        }
+              }
+      .pickerStyle(MenuPickerStyle())
+    }
+  }
+}
+
+struct ModifyMainInformationView_Previews: PreviewProvider {
+    @State static var mainInformation = MainInformation(name: "Test Name",
+                                                        description: "Test Description",
+                                                        author: "Test Author",
+                                                        category: .breakfast)
+    @State static var emptyInformation = MainInformation(name: "", description: "", author: "", category: .breakfast)
+        
+    static var previews: some View {
+        ModifyMainInformationView(mainInformation: $mainInformation)
+        ModifyMainInformationView(mainInformation: $emptyInformation)
+    }
+}
